017c4f6889584a19a168aa729b21642f72c33706
Carolina Eskenazi <carolinae3@al.insper.edu.br>
2025-06-02 08:10:34 -0300

    getter e set

--- src/main/java/com/gonzaguinha/demo/alunos/InfoMedica.java
+++ src/main/java/com/gonzaguinha/demo/alunos/InfoMedica.java
@@ -16,5 +16,67 @@ public class InfoMedica {
     private boolean atencaoPsico;
     private boolean outros;
 
+    public int getFaltasMed() {
+        return faltasMed;
+    }
 
+    public void setFaltasMed(int faltasMed) {
+        this.faltasMed = faltasMed;
+    }
+
+    public int getTotalFaltas() {
+        return totalFaltas;
+    }
+
+    public void setTotalFaltas(int totalFaltas) {
+        this.totalFaltas = totalFaltas;
+    }
+
+    public String getCondSaude() {
+        return condSaude;
+    }
+
+    public void setCondSaude(String condSaude) {
+        this.condSaude = condSaude;
+    }
+
+    public boolean isRestricaoAlim() {
+        return restricaoAlim;
+    }
+
+    public void setRestricaoAlim(boolean restricaoAlim) {
+        this.restricaoAlim = restricaoAlim;
+    }
+
+    public boolean isMedicacao() {
+        return medicacao;
+    }
+
+    public void setMedicacao(boolean medicacao) {
+        this.medicacao = medicacao;
+    }
+
+    public boolean isDeficiencia() {
+        return deficiencia;
+    }
+
+    public void setDeficiencia(boolean deficiencia) {
+        this.deficiencia = deficiencia;
+    }
+
+    public boolean isAtencaoPsico() {
+        return atencaoPsico;
+    }
+
+    public void setAtencaoPsico(boolean atencaoPsico) {
+        this.atencaoPsico = atencaoPsico;
+    }
+
+    public boolean isOutros() {
+        return outros;
+    }
+
+    public void setOutros(boolean outros) {
+        this.outros = outros;
+    }
 }
--- src/main/java/com/gonzaguinha/demo/turmas/Turma.java
+++ src/main/java/com/gonzaguinha/demo/turmas/Turma.java
@@ -21,5 +21,59 @@ public class Turma {
 
     private List<String> alunos; // IDs dos alunos
 
+    public String getId() {
+        return id;
+    }
 
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public List<String> getAlunos() {
+        return alunos;
+    }
+
+    public void setAlunos(List<String> alunos) {
+        this.alunos = alunos;
+    }
+
+    public boolean isNoite() {
+        return noite;
+    }
+
+    public void setNoite(boolean noite) {
+        this.noite = noite;
+    }
+
+    public boolean isTarde() {
+        return tarde;
+    }
+
+    public void setTarde(boolean tarde) {
+        this.tarde = tarde;
+    }
+
+    public boolean isManha() {
+        return manha;
+    }
+
+    public void setManha(boolean manha) {
+        this.manha = manha;
+    }
+
+    public String getSerie() {
+        return serie;
+    }
+
+    public void setSerie(String serie) {
+        this.serie = serie;
+    }
+
+    public String getNomeTurma() {
+        return nomeTurma;
+    }
+
+    public void setNomeTurma(String nomeTurma) {
+        this.nomeTurma = nomeTurma;
+    }
 }
